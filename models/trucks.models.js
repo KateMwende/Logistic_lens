345f5393051a07e0e423e388448e84acfe1af8c6
KateMwende <kmwende419@gmail.com>
2024-06-10 15:59:16 +0300

    Changed id field

--- models/trucks.models.js
+++ models/trucks.models.js
@@ -5,7 +5,7 @@ const { bagSchema } = require('./bags.models');
 
 //define truck schema
 const truckSchema = new mongoose.Schema({
-    _id: { type: String, default: uuidv4 },
+    id: { type: String, default: uuidv4 },
     driver: String,
     number_plate: { type: String, unique: true, required: true },
     capacityTonnes: { type: Number, required: true }, //Capacity in tonnes
